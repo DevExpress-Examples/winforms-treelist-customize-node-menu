65e2dee97d1f4a7961e907b3135d049def7785fa
DevExpressExampleBot <fakeEmail>
2023-09-03 15:16:03 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,46 +1,46 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace NodeContextMenu
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-            Me.components = New System.ComponentModel.Container
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
+
+            MyBase.Dispose(disposing)
+        End Sub
+
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
             Me.barManager1 = New DevExpress.XtraBars.BarManager(Me.components)
-            Me.barDockControlTop = New DevExpress.XtraBars.BarDockControl
-            Me.barDockControlBottom = New DevExpress.XtraBars.BarDockControl
-            Me.barDockControlLeft = New DevExpress.XtraBars.BarDockControl
-            Me.barDockControlRight = New DevExpress.XtraBars.BarDockControl
-            Me.bbEdit = New DevExpress.XtraBars.BarButtonItem
-            Me.bbAddChild = New DevExpress.XtraBars.BarButtonItem
-            Me.bbDelete = New DevExpress.XtraBars.BarButtonItem
-            Me.treeList1 = New DevExpress.XtraTreeList.TreeList
-            CType(Me.barManager1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.treeList1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.barDockControlTop = New DevExpress.XtraBars.BarDockControl()
+            Me.barDockControlBottom = New DevExpress.XtraBars.BarDockControl()
+            Me.barDockControlLeft = New DevExpress.XtraBars.BarDockControl()
+            Me.barDockControlRight = New DevExpress.XtraBars.BarDockControl()
+            Me.bbEdit = New DevExpress.XtraBars.BarButtonItem()
+            Me.bbAddChild = New DevExpress.XtraBars.BarButtonItem()
+            Me.bbDelete = New DevExpress.XtraBars.BarButtonItem()
+            Me.treeList1 = New DevExpress.XtraTreeList.TreeList()
+            CType((Me.barManager1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.treeList1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
-            '
-            'barManager1
-            '
+            ' 
+            ' barManager1
+            ' 
             Me.barManager1.DockControls.Add(Me.barDockControlTop)
             Me.barManager1.DockControls.Add(Me.barDockControlBottom)
             Me.barManager1.DockControls.Add(Me.barDockControlLeft)
@@ -48,55 +48,58 @@ Namespace NodeContextMenu
             Me.barManager1.Form = Me
             Me.barManager1.Items.AddRange(New DevExpress.XtraBars.BarItem() {Me.bbEdit, Me.bbAddChild, Me.bbDelete})
             Me.barManager1.MaxItemId = 3
-            '
-            'barDockControlTop
-            '
+            ' 
+            ' barDockControlTop
+            ' 
             Me.barDockControlTop.CausesValidation = False
             Me.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top
             Me.barDockControlTop.Location = New System.Drawing.Point(0, 0)
             Me.barDockControlTop.Size = New System.Drawing.Size(573, 0)
-            '
-            'barDockControlBottom
-            '
+            ' 
+            ' barDockControlBottom
+            ' 
             Me.barDockControlBottom.CausesValidation = False
             Me.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom
             Me.barDockControlBottom.Location = New System.Drawing.Point(0, 393)
             Me.barDockControlBottom.Size = New System.Drawing.Size(573, 0)
-            '
-            'barDockControlLeft
-            '
+            ' 
+            ' barDockControlLeft
+            ' 
             Me.barDockControlLeft.CausesValidation = False
             Me.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left
             Me.barDockControlLeft.Location = New System.Drawing.Point(0, 0)
             Me.barDockControlLeft.Size = New System.Drawing.Size(0, 393)
-            '
-            'barDockControlRight
-            '
+            ' 
+            ' barDockControlRight
+            ' 
             Me.barDockControlRight.CausesValidation = False
             Me.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right
             Me.barDockControlRight.Location = New System.Drawing.Point(573, 0)
             Me.barDockControlRight.Size = New System.Drawing.Size(0, 393)
-            '
-            'bbEdit
-            '
+            ' 
+            ' bbEdit
+            ' 
             Me.bbEdit.Caption = "Edit"
             Me.bbEdit.Id = 0
             Me.bbEdit.Name = "bbEdit"
-            '
-            'bbAddChild
-            '
+            AddHandler Me.bbEdit.ItemClick, New DevExpress.XtraBars.ItemClickEventHandler(AddressOf Me.bbEdit_ItemClick)
+            ' 
+            ' bbAddChild
+            ' 
             Me.bbAddChild.Caption = "Add child"
             Me.bbAddChild.Id = 1
             Me.bbAddChild.Name = "bbAddChild"
-            '
-            'bbDelete
-            '
+            AddHandler Me.bbAddChild.ItemClick, New DevExpress.XtraBars.ItemClickEventHandler(AddressOf Me.bbAddChild_ItemClick)
+            ' 
+            ' bbDelete
+            ' 
             Me.bbDelete.Caption = "Delete"
             Me.bbDelete.Id = 2
             Me.bbDelete.Name = "bbDelete"
-            '
-            'treeList1
-            '
+            AddHandler Me.bbDelete.ItemClick, New DevExpress.XtraBars.ItemClickEventHandler(AddressOf Me.bbDelete_ItemClick)
+            ' 
+            ' treeList1
+            ' 
             Me.treeList1.Dock = System.Windows.Forms.DockStyle.Fill
             Me.treeList1.Location = New System.Drawing.Point(0, 0)
             Me.treeList1.MenuManager = Me.barManager1
@@ -104,10 +107,12 @@ Namespace NodeContextMenu
             Me.treeList1.OptionsBehavior.AutoFocusNewNode = True
             Me.treeList1.Size = New System.Drawing.Size(573, 393)
             Me.treeList1.TabIndex = 4
-            '
-            'Form1
-            '
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            AddHandler Me.treeList1.PopupMenuShowing, New DevExpress.XtraTreeList.PopupMenuShowingEventHandler(AddressOf Me.treeList1_PopupMenuShowing)
+            AddHandler Me.treeList1.HiddenEditor, New System.EventHandler(AddressOf Me.treeList1_HiddenEditor)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
             Me.ClientSize = New System.Drawing.Size(573, 393)
             Me.Controls.Add(Me.treeList1)
@@ -117,23 +122,28 @@ Namespace NodeContextMenu
             Me.Controls.Add(Me.barDockControlTop)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            CType(Me.barManager1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.treeList1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.barManager1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.treeList1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-		#End Region
+#End Region
+        Private barManager1 As DevExpress.XtraBars.BarManager
 
-		Private barManager1 As DevExpress.XtraBars.BarManager
-		Private barDockControlTop As DevExpress.XtraBars.BarDockControl
-		Private barDockControlBottom As DevExpress.XtraBars.BarDockControl
-		Private barDockControlLeft As DevExpress.XtraBars.BarDockControl
-		Private barDockControlRight As DevExpress.XtraBars.BarDockControl
-		Private WithEvents bbEdit As DevExpress.XtraBars.BarButtonItem
-		Private WithEvents bbAddChild As DevExpress.XtraBars.BarButtonItem
-		Private WithEvents bbDelete As DevExpress.XtraBars.BarButtonItem
-        Private WithEvents treeList1 As DevExpress.XtraTreeList.TreeList
-	End Class
+        Private barDockControlTop As DevExpress.XtraBars.BarDockControl
+
+        Private barDockControlBottom As DevExpress.XtraBars.BarDockControl
+
+        Private barDockControlLeft As DevExpress.XtraBars.BarDockControl
+
+        Private barDockControlRight As DevExpress.XtraBars.BarDockControl
+
+        Private bbEdit As DevExpress.XtraBars.BarButtonItem
+
+        Private bbAddChild As DevExpress.XtraBars.BarButtonItem
+
+        Private bbDelete As DevExpress.XtraBars.BarButtonItem
+
+        Private treeList1 As DevExpress.XtraTreeList.TreeList
+    End Class
 End Namespace
-
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,27 +1,27 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraTreeList
 Imports DevExpress.XtraTreeList.Nodes
+Imports DevExpress.XtraTreeList.Menu
 
 Namespace NodeContextMenu
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
+
+    Public Partial Class Form1
+        Inherits Form
+
+        Public Sub New()
             InitializeComponent()
             InitTreeList()
-		End Sub
+        End Sub
 
         Private Sub InitTreeList()
-            Dim TempXViews As DevExpress.XtraTreeList.Design.XViews = New DevExpress.XtraTreeList.Design.XViews(treeList1)
+            Dim tmp_XViews = New Design.XViews(treeList1)
             For i As Integer = 1 To treeList1.Columns.Count - 1
                 treeList1.Columns(i).Visible = False
-            Next i
+            Next
         End Sub
 
         Private Sub bbEdit_ItemClick(ByVal sender As Object, ByVal e As EventArgs)
@@ -32,7 +32,6 @@ Namespace NodeContextMenu
         Private Sub bbAddChild_ItemClick(ByVal sender As Object, ByVal e As EventArgs)
             Dim newNode As TreeListNode = treeList1.AppendNode(Nothing, treeList1.FocusedNode)
             newNode.SetValue(0, "New Node")
-            treeList1.FocusedNode = newNode
             treeList1.OptionsBehavior.Editable = Not treeList1.OptionsBehavior.Editable
             treeList1.ShowEditor()
         End Sub
@@ -41,13 +40,13 @@ Namespace NodeContextMenu
             treeList1.DeleteNode(treeList1.FocusedNode)
         End Sub
 
-        Private Sub treeList1_HiddenEditor(ByVal sender As Object, ByVal e As System.EventArgs) Handles treeList1.HiddenEditor
+        Private Sub treeList1_HiddenEditor(ByVal sender As Object, ByVal e As EventArgs)
             treeList1.OptionsBehavior.Editable = False
         End Sub
 
-        Private Sub treeList1_PopupMenuShowing(ByVal sender As System.Object, ByVal e As DevExpress.XtraTreeList.PopupMenuShowingEventArgs) Handles treeList1.PopupMenuShowing
-            If TypeOf e.Menu Is DevExpress.XtraTreeList.Menu.TreeListNodeMenu Then
-                treeList1.FocusedNode = (CType(e.Menu, DevExpress.XtraTreeList.Menu.TreeListNodeMenu)).Node
+        Private Sub treeList1_PopupMenuShowing(ByVal sender As Object, ByVal e As PopupMenuShowingEventArgs)
+            If TypeOf e.Menu Is TreeListNodeMenu Then
+                treeList1.FocusedNode = CType(e.Menu, TreeListNodeMenu).Node
                 e.Menu.Items.Add(New DevExpress.Utils.Menu.DXMenuItem("Edit", AddressOf bbEdit_ItemClick))
                 e.Menu.Items.Add(New DevExpress.Utils.Menu.DXMenuItem("Add child", AddressOf bbAddChild_ItemClick))
                 e.Menu.Items.Add(New DevExpress.Utils.Menu.DXMenuItem("Delete", AddressOf bbDelete_ItemClick))
